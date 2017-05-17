b3c3ebdf5c69056b1a63b661cb33b0c66c9c1f21
Allen Leonard (Salsalabs) <aleonard@salsalabs.com>
2017-05-17 01:27:29 +0000

    Moved PHP stuff to another project.

--- php/supporter_search_via_api.bash
+++
@@ -1,6 +0,0 @@
-curl -H "Content-Type: application/json" \
--H 'authToken: Dp5aFQ3HMMz6ARRYQlKGxoeOuG8X7l2N6toEx5o0i7nx31z3Vzq-Oq3DdXYYG6hzY8aFY0lnQGpInF0gIYsSRAQyIIlwYKGdw7uUU4XEyGBfCNO7MCmhS37rsOrWncnKUB0tB6HUgp-QiMcI0wxh2w' \
--X POST \
--d '{ "payload": { "count":10, "offset":0, "email": "blaise.dufrain@saltermitchell.com", "modifiedFrom": "2017-05-01T16:22:06.978Z" }}' \
-https://api.salsalabs.org/api/integration/ext/v1/supporters/search
-
